2f6d05a06bb18ce3451262a88e2ac5537bf49fec
Adrian <Adrian@DESKTOP-4IDCAJE>
2025-10-06 17:04:22 +0200

    Mi primer commit

---
+++ src/test/CalculadoraTest.java
@@ -0,0 +1,52 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void Sumatest() {
+		assertEquals(5, Calculadora.suma(2,3));
+	}
+	@Test
+	void Restatest() {
+		assertEquals(5, Calculadora.resta(10,5));
+	}
+	@Test
+	void Multiplicaciontest() {
+		assertEquals(6, Calculadora.multiplicacion(3,2));
+		
+	}
+	@Test
+	void Divisiontest() {
+		assertEquals(6, Calculadora.division(12,2));
+	}/*
+	@Test
+	void DivisionEntreCerotest() {
+		assertEquals(-1, Calculadora.division(12,0));
+	}
+*/
+
+}
